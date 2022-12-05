5d0374d674a6ec4295f8afbdc6f4b7035070e611
TheoBIET <dev.theobiet@gmail.com>
2022-12-05 20:11:38 +0100

    🎨 | gallery component pre-integration

--- src/components/Home/Gallery.js
+++ src/components/Home/Gallery.js
@@ -1,7 +1,29 @@
 export default function Gallery() {
+
+    const sampleCount = 9;
+
     return (
         <div className="Gallery">
-            Hello World i'm Gallery
+            <div className="Gallery__content">
+                <h2 className="Gallery__content__title">Manga Gallery</h2>
+                <p className="Gallery__content__paragraph">
+                    I love to watch some anime on my free-time, you can see below a gallery of a lot of them.
+                    If vous vous posez la question, Oui j’ai ajouté cette section afin d’avoir une excuse
+                    pour faire un effet supplémentaire.
+                </p>
+            </div>
+            <ul className="Gallery__list">
+                {Array.from(Array(sampleCount).keys()).map((i) => (
+                    <li className="Gallery__list__item" key={i}>
+                        <img
+                            className="Gallery__list__item__image"
+                            src={`https://picsum.photos/seed/${i * 2111}/200/300`}
+                            alt="Manga"
+                        />
+                        <div className="Gallery__list__item__overlay">PLACEHOLDER</div>
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
