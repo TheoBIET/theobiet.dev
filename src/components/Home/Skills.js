c3e4d5aedfb7caeb4bf928403acc35225db4f72b
TheoBIET <dev.theobiet@gmail.com>
2022-12-05 18:43:26 +0100

    🎨 | skills component pre-integration

--- src/components/Home/Skills.js
+++ src/components/Home/Skills.js
@@ -1,7 +1,35 @@
+import { useState } from "react";
+
 export default function Skills() {
+    const skillsList = [
+        'Languages',
+        'Technologies',
+        'Tools',
+        'Softwares',
+    ]
+
+    const [selectedSkill, setSelectedSkill] = useState(skillsList[0]);
+
     return (
         <div className="Skills">
-            Hello World i'm Skills
+            <div className="Skills__computer">
+                <div className="Skills__computer__renderer">
+                </div>
+            </div>
+            <div className="Skills__content">
+                <h2 className="Skills__content__title">Skills</h2>
+                <ul className="Skills__content__list">
+                    {skillsList.map((skill, index) => (
+                        <li
+                            key={index}
+                            className={`Skills__content__list__item ${selectedSkill === skill ? '--active' : ''}`}
+                            onClick={() => setSelectedSkill(skill)}
+                        >
+                            {skill}
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </div>
     )
 }
