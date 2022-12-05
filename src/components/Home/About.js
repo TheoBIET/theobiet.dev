5a8a6347cabf9f4db73ae3f4315405d33b7f654b
TheoBIET <dev.theobiet@gmail.com>
2022-12-05 17:45:53 +0100

    🎨 | about me component pre-integration

--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -1,7 +1,26 @@
 export default function About() {
     return (
         <div className="About">
-            Hello World i'm About
+            <div className="About__title">
+                <h2>About me</h2>
+            </div>
+            <div className="About__content">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Donec a diam lectus. Sed sit amet ipsum mauris. Maecenas
+                    congue ligula ac quam viverra nec consectetur ante
+                    hendrerit. Donec et mollis dolor. Praesent et diam eget
+                    libero egestas mattis sit amet vitae augue. Nam tincidunt
+                    congue enim, ut porta lorem lacinia consectetur. Donec ut
+                    libero sed arcu vehicula ultricies a non tortor. Lorem
+                    ipsum dolor sit amet, consectetur adipiscing elit. Aenean
+                    ut gravida lorem. Ut turpis felis, pulvinar a semper sed,
+                    adipiscing id dolor. Pellentesque auctor nisi id magna
+                </p>
+            </div>
+            <div className="About__character">
+                {/* TODO: Add ThreeJS Integration */}
+            </div>
         </div>
     )
 }
